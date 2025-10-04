4c8e6afd80c6cfd7a2c65c510e3fadabc2b8cb08
VitoriaMaglio <vitoriamaglio@gmail.com>
2025-10-04 11:51:47 -0300

    Iniciando projeto com spring boot.

---
+++ src/test/java/com/estudando/spring/Screenmatch/ScreenmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudando.spring.Screenmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
